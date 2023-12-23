ad3ebe250f1059311b16366a2d3fae918be7d11f
TC <nothing@nothing.com>
2023-12-23 22:34:19 +1300

    Working calculator made. Previous equations also displayed

--- scripts/code.js
+++ scripts/code.js
@@ -24,44 +24,100 @@ function exp(a,b){
   return a ** b;
 }
 
-function operate(a,operator,b){
+function operate(a, b, operator){
+  // equationString
+  // let [a, b, operator] = getOpsFromString(equationString);
+  let sum = 0;
   switch(operator) {
     case '+':
-      return add(a,b);
+      sum = add(a,b);
       break;
     case '-':
-      return sub(a,b);
+      sum = sub(a,b);
       break;
     case '*':
-      return mul(a,b);
+      sum = mul(a,b);
       break;
     case '/':
-      return div(a,b);
+      sum = div(a,b);
       break;
     case '%':
-      return mod(a,b);
+      sum = mod(a,b);
       break;
     case '^':
-      return exp(a,b);
+      sum = exp(a,b);
       break;
     default:
-      return 'ERR';
+      sum = 'ERR';
   }
+  return(sum);
+}
+
+function getOpsFromString(equationString){
+  const eqArr = equationString.match(/([-+])?([0-9]+)([+-/*%])([0-9]+)/);
+  let op1 = eqArr[1] ? parseFloat(eqArr[1] + eqArr[2]) : parseFloat(eqArr[2]); // Allow for Negatives
+  let op2 = parseFloat(eqArr[4]);
+  let operator = eqArr[3];  
+  return [op1, op2, operator];
+}
+
+const firstOpDisplay = document.querySelector('.display-operand1')
+const operatorDisplay = document.querySelector('.display-operator');
+const secondOpDisplay = document.querySelector('.display-operand2');
+function updateDisplays(buttonId){
+  const firstOp = parseFloat(firstOpDisplay.textContent);
+  const secondOp = parseFloat(secondOpDisplay.textContent);
+  const operator = operatorDisplay.textContent;
+  if (!isNaN(buttonId)) {
+    secondOpDisplay.textContent += buttonId;
+    return;
+  }
+
+  if (firstOpDisplay.textContent && secondOpDisplay.textContent) {
+    if (buttonId = '=') {
+      clearDisplays();
+      secondOpDisplay.textContent = operate(firstOp, secondOp, operator);
+      return;
+    }
+    firstOpDisplay.textContent = operate(firstOp, secondOp, operator)
+  } else if (!secondOp) {
+  } else {
+    firstOpDisplay.textContent = secondOp;
+  }
+  secondOpDisplay.textContent = '';
+  operatorDisplay.textContent = buttonId;
+};
+
+function clearDisplays(){
+  firstOpDisplay.textContent = '';
+  operatorDisplay.textContent = '';
+  secondOpDisplay.textContent = '';
 }
 
 // ----
-let num1;
-let num2;
-let operator;
 
 const buttons = document.querySelectorAll('.buttons');
-const display = document.querySelector('.display');
 buttons.forEach( button => {
-  if (button.id === "clear" || button.id === "=") {
-
+  if (button.id === 'clr') {
+    button.onclick = clearDisplays;
   } else {
     button.addEventListener( 'click', () => {
-      display.textContent += button.id;
+      updateDisplays(button.id);
     })
   }
 });
+
+
+// if (button.id === "=") {
+//   button.addEventListener('click', () => {
+//     operate(display.textContent);
+//   })
+// } else if (button.id === "clr" ) {
+//   button.addEventListener( 'click', () => {
+//     display.textContent = '';
+//   })
+// } else {
+//   button.addEventListener( 'click', () => {
+//     updateDisplay(button.id);
+//   })
+// }
